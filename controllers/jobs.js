20af29acbe30aeaf9ba4d6ecd45756106900509d
Islam Aboamh <75704011+I-A11@users.noreply.github.com>
2023-05-25 10:02:01 +1000

    Create controllers and routers.

---
+++ controllers/jobs.js
@@ -0,0 +1,21 @@
+const getAllJobs = async (req, res) => {
+  console.log("get all jobs");
+};
+
+const getJob = async (req, res) => {
+  console.log("get job");
+};
+
+const createJob = async (req, res) => {
+  console.log("create job");
+};
+
+const updateJob = async (req, res) => {
+  console.log("updateJob");
+};
+
+const deleteJob = async (req, res) => {
+  console.log("delete job");
+};
+
+module.exports = { getAllJobs, getJob, createJob, updateJob, deleteJob };
