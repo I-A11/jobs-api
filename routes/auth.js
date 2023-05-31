d2a2dd677755aab4232f906b17016ea272fd1827
Islam Aboamh <75704011+I-A11@users.noreply.github.com>
2023-05-31 11:16:07 +1000

    Hash user password.

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const { register, controller, login } = require("../controllers/auth.js");
+const { register, login } = require("../controllers/auth.js");
 
 router.post("/register", register);
 router.post("/login", login);
